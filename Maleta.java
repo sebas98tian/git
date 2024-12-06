38783e113d94d88b42c4ea648d02569c4e90c21e
sebas98tian <juansebastianbernal2@gmail.com>
2024-12-06 01:14:18 -0500

    Un nuevo cambio

--- Maleta.java
+++ Maleta.java
@@ -1,5 +1,8 @@
+import java.util.HashMap;
 public class Maleta {
     public static void main(String[] args) {
+        HashMap<String,Integer> mapa=new HashMap<>();
+        System.out.println(mapa);
         System.out.println("hola Git");
     }
 }
